95c1b20980d3c83b211d698e771f858bde1cc240
gipsohlavi <gipsohlavi@gmail.com>
2025-08-17 23:09:09 +0900

    db接続時のuser,passwordの変更

--- dbconnect.php
+++ dbconnect.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $db = new PDO('mysql:dbname=hairtest;host=127.0.0.1;charset=utf8', 'staff','passward');
+    $db = new PDO('mysql:dbname=hairtest;host=localhost;charset=utf8', 'root','');
 } catch (PDOException $e) {
     echo 'DB接続エラー： ' . $e->getMessage();
 }
